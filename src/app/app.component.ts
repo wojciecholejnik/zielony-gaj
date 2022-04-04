b500e82945ad3affcdfa6d1535c851747066bd3b
wole <wojciech.olejnik@inetum.com>
2022-04-04 16:38:35 +0200

    firs draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'zielony-gaj';
+
+  goToBarberia(){
+    window.open('https://booksy.com/pl-pl/122799_barberia-fryzjer-meski-i-golibroda_barber-shop_16979_gadki#ba_s=sh_1', '_blank')
+  }
+
+  openFacebook(){
+    window.open('https://www.facebook.com/Zielony-Gaj-100625792599151', '_blank')
+  }
+
+  openInsta(){
+    window.open('https://www.instagram.com/explore/locations/100625792599151/zielony-gaj/?hl=pl', '_blank')
+  }
 }
